083bcdbc92e2585ade7b63bae89830f61b0ff181
lauri280 <laurileas@yahoo.com>
2018-06-07 18:14:34 +0300

    Frontend stuff and

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ app.get('/', function(req, res) {
 
 app.post('/', function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&units=imperial&appid=${apiKey}`;
 
     request(url, function(err, response, body) {
         if (err) {
             res.render('index', {weather: null, error: 'Error, please try again'});
         } else {
             let weather = JSON.parse(body);
+
             if (weather.main == undefined) {
                 res.render('index', {weather: null, error: 'Error, pleas try again'});
             } else {
